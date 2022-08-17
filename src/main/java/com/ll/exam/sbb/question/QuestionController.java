d80b18ce31073b7e2334189f05b9ccaf87b2a338
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2022-08-17 13:23:10 +0900

    질문 등록하기 생성

--- src/main/java/com/ll/exam/sbb/question/QuestionController.java
+++ src/main/java/com/ll/exam/sbb/question/QuestionController.java
@@ -4,9 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -36,11 +34,15 @@ public class QuestionController {
         model.addAttribute("question",question);
         return "question_detail";
     }
+    @GetMapping("/create")
+    public String questionCreate() {
+        return "question_form"; // 질문 저장후 질문목록으로 이동
+    }
 
-    @RequestMapping("/list2")
-    @ResponseBody
-    public String list2() {
-        return "하하111777";
+    @PostMapping("/create")
+    public String questionCreate(String subject, String content) {
+        questionService.create(subject, content);
+        return "redirect:/question/list"; // 질문 저장후 질문목록으로 이동
     }
 
 }
