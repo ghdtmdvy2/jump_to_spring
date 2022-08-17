5300e4a3cddfb12a80116f50e33f6921d1bfb3ba
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2022-08-17 13:56:03 +0900

    질문 등록 validation 추가

--- src/main/java/com/ll/exam/sbb/question/QuestionController.java
+++ src/main/java/com/ll/exam/sbb/question/QuestionController.java
@@ -1,16 +1,27 @@
 package com.ll.exam.sbb.question;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RequestMapping("/question")
 @Controller
 @RequiredArgsConstructor // 생성자 주입
+// 컨트롤러는 Repository가 있는지 몰라야 한다.
+// 서비스는 웹브라우저라는것이 이 세상에 존재하는지 몰라야 한다.
+// 리포지터리는 서비스를 몰라야 한다.
+// 서비스는 컨트롤러를 몰라야 한다.
+// DB는 리포지터리를 몰라야 한다.
+// SPRING DATA JPA는 MySQL을 몰라야 한다.
+// SPRING DATA JPA(리포지터리) -> JPA -> 하이버네이트 -> JDBC -> MySQL Driver -> MySQL
 public class QuestionController {
     // @Autowired // 필드 주입
     private final QuestionService questionService;
@@ -28,21 +39,26 @@ public class QuestionController {
     }
 
     @RequestMapping("/detail/{id}")
-    // 이 자리에 @ResponseBody가 없으면 resources/question_list/question_list.html 파일을 뷰로 삼는다.
     public String detail(Model model, @PathVariable int id) {
         Question question = questionService.getQuestion(id);
-        model.addAttribute("question",question);
+
+        model.addAttribute("question", question);
+
         return "question_detail";
     }
+
     @GetMapping("/create")
-    public String questionCreate() {
-        return "question_form"; // 질문 저장후 질문목록으로 이동
+    public String questionCreate(QuestionForm questionForm) {
+        return "question_form";
     }
 
     @PostMapping("/create")
-    public String questionCreate(String subject, String content) {
-        questionService.create(subject, content);
+    public String questionCreate(Model model, @Valid QuestionForm questionForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "question_form";
+        }
+
+        questionService.create(questionForm.getSubject(), questionForm.getContent());
         return "redirect:/question/list"; // 질문 저장후 질문목록으로 이동
     }
-
 }
