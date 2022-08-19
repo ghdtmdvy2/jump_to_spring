b3288666b4487ffa871bf80cdcd356df7dcca693
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2022-08-19 11:45:03 +0900

    자기 사용자 정보를 확인을 위한 URL Mapping 추가

--- src/main/java/com/ll/exam/sbb/user/UserController.java
+++ src/main/java/com/ll/exam/sbb/user/UserController.java
@@ -2,11 +2,14 @@ package com.ll.exam.sbb.user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.validation.Valid;
 
@@ -60,5 +63,11 @@ public class UserController {
     @GetMapping("/login")
     public String login() {
         return "login_form";
+    }
+    @GetMapping("/auth")
+    @ResponseBody
+    public Authentication auth(){
+        return SecurityContextHolder.getContext()
+                .getAuthentication();
     }
 }
--- src/main/java/com/ll/exam/sbb/user/UserRole.java
+++ src/main/java/com/ll/exam/sbb/user/UserRole.java
@@ -12,4 +12,6 @@ public enum UserRole {
     }
 
     private String value;
+
+
 }
