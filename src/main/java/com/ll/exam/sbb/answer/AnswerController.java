659489c2cd5a0d59a449a803c7368ce44337c0cf
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2022-08-23 15:21:53 +0900

    추천 할 시 앵커로 인해 자동으로 스크롤

--- src/main/java/com/ll/exam/sbb/answer/AnswerController.java
+++ src/main/java/com/ll/exam/sbb/answer/AnswerController.java
@@ -11,7 +11,10 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
@@ -25,30 +28,29 @@ public class AnswerController {
     private final AnswerService answerService;
     private final UserService userService;
 
+    @PreAuthorize("isAuthenticated()")
     @PostMapping("/create/{id}")
     public String detail(Principal principal, Model model, @PathVariable long id, @Valid AnswerForm answerForm, BindingResult bindingResult) {
         Question question = this.questionService.getQuestion(id);
 
-        if (bindingResult.hasErrors()){
-            model.addAttribute("question",question);
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("question", question);
             return "question_detail";
         }
+
         SiteUser siteUser = userService.getUser(principal.getName());
 
         // 답변 등록 시작
         Answer answer = answerService.create(question, answerForm.getContent(), siteUser);
         // 답변 등록 끝
-        return "redirect:/question/detail/%d#answer_%d".formatted(id,answer.getId());
+
+        return "redirect:/question/detail/%d#answer_%d".formatted(id, answer.getId());
     }
 
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/modify/{id}")
     public String answerModify(AnswerForm answerForm, @PathVariable("id") Long id, Principal principal) {
         Answer answer = answerService.getAnswer(id);
-
-        if ( answer == null ) {
-            throw new DataNotFoundException("데이터가 없습니다.");
-        }
 
         if (!answer.getAuthor().getUsername().equals(principal.getName())) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
@@ -74,16 +76,13 @@ public class AnswerController {
 
         answerService.modify(answer, answerForm.getContent());
 
-        return "redirect:/question/detail/%d".formatted(answer.getQuestion().getId());
+        return "redirect:/question/detail/%d#answer_%d".formatted(answer.getQuestion().getId(), answer.getId());
     }
+
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/delete/{id}")
-    public String answerDelete(@PathVariable("id") Long id, Principal principal) {
+    public String answerDelete(Principal principal, @PathVariable("id") Long id) {
         Answer answer = answerService.getAnswer(id);
-
-        if ( answer == null ) {
-            throw new DataNotFoundException("데이터가 없습니다.");
-        }
 
         if (!answer.getAuthor().getUsername().equals(principal.getName())) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제권한이 없습니다.");
@@ -101,6 +100,6 @@ public class AnswerController {
         SiteUser siteUser = userService.getUser(principal.getName());
 
         answerService.vote(answer, siteUser);
-        return "redirect:/question/detail/%d".formatted(answer.getQuestion().getId());
+        return "redirect:/question/detail/%d#answer_%d".formatted(answer.getQuestion().getId(), answer.getId());
     }
 }
