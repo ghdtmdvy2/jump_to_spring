9c3729fe17d541f2479c5aff22c191316d26e350
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2022-08-25 11:41:52 +0900

    검색 폼 추가

--- src/test/java/com/ll/exam/sbb/QuestionRepositoryTests.java
+++ src/test/java/com/ll/exam/sbb/QuestionRepositoryTests.java
@@ -154,7 +154,7 @@ public class QuestionRepositoryTests {
 
     @Test
     void createManySampleData() {
-        boolean run = false;
+        boolean run = true;
 
         if (run == false) return;
 
